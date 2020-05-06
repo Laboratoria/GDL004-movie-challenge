891e76c2014e396b11f35c5d8f67bfbe4f4e2b7c
Maria Lujan Navarro <lujannavarro26@gmail.com>
2020-05-05 19:56:54 -0500

    delete dead code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-let list = document.getElementById("series_container");
-const series_number = 8;
-let serieList = [1, 2, 3, 4];
 let moviesPromises = [];
 for (let i = 1; i <= 8; i++) {
   let newKey = "";
