a108eccf80d228d40010f540334a93be974c24a1
biancardona <biancardona.rivera@gmail.com>
2020-05-06 21:16:04 -0500

    fixing problems in searching button when there is no coincidence

--- src/Views/AllMovies.js
+++ src/Views/AllMovies.js
@@ -4,20 +4,27 @@ import { useFetch } from '../Components/UseFetch';
 
 export default function DataLoader({ match }) {
   const data = useFetch(match.params.id);
+  if (data !== undefined) {
+    return (
+      <div>
+        <ul>
+          {data.map((el) => (
+            <li>
+              <Link key={el.imdbID} to={`/moviedetail/${el.imdbID}`}>
+                <img src={el.Poster} alt="" />
+                <li>
+                  {el.Title}
+                </li>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
   return (
     <div>
-      <ul>
-        {data.map((el) => (
-          <li>
-            <Link key={el.imdbID} to={`/moviedetail/${el.imdbID}`}>
-              <img src={el.Poster} alt="" />
-              <li>
-                {el.Title}
-              </li>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <h1> Ups¡ We couldnt find any movie that matches your search.</h1>
     </div>
   );
 }
